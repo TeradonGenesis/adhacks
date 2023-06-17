46928484d090614d04f28074ba2c27b5798b2aab
jbyomuhangi <jbyomuhangi@gmail.com>
2023-06-17 15:11:27 +0800

    Created ActionButton component

---
+++ adhacks-web-client/src/components/Buttons/ActionButton/ActionButton.js
@@ -0,0 +1,34 @@
+import { styled, Typography } from "@mui/material";
+import React, { useMemo } from "react";
+
+const ActionButton = ({ label, variant = "filled" }) => {
+  const { ActionButtonContainer, Label } = useMemo(() => {
+    const isFilled = variant === "filled";
+
+    const ActionButtonContainer = styled("button")(({ theme }) => ({
+      backgroundColor: isFilled
+        ? theme.palette.primary.main
+        : theme.palette.common.white,
+
+      padding: `${theme.spacing(1)} ${theme.spacing(2)}`,
+      borderRadius: "30px",
+      border: isFilled ? "unset" : `2px solid ${theme.palette.primary.main}`,
+    }));
+
+    const Label = styled(Typography)(({ theme }) => ({
+      fontWeight: "bold",
+      fontSize: "1.2rem",
+      color: isFilled ? theme.palette.common.white : theme.palette.primary.main,
+    }));
+
+    return { ActionButtonContainer, Label };
+  }, [variant]);
+
+  return (
+    <ActionButtonContainer sx={{}}>
+      <Label>{label}</Label>
+    </ActionButtonContainer>
+  );
+};
+
+export default ActionButton;
--- adhacks-web-client/src/pages/Home/HomePage.js
+++ adhacks-web-client/src/pages/Home/HomePage.js
@@ -1,7 +1,30 @@
+import { Box, styled } from "@mui/material";
 import React from "react";
 
+import ActionButton from "@/components/Buttons/ActionButton";
+
+const HomePageContainer = styled(Box)(({ theme }) => ({
+  padding: `${theme.spacing(5)}`,
+}));
+
+const ToolbarContainer = styled(Box)(({ theme }) => ({
+  display: "flex",
+  justifyContent: "space-between",
+}));
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <HomePageContainer>
+      <ToolbarContainer>
+        <Box>Search</Box>
+        <Box>
+          <ActionButton label="Add company" variant="outlined" />
+        </Box>
+      </ToolbarContainer>
+
+      <Box>table</Box>
+    </HomePageContainer>
+  );
 };
 
 export default HomePage;
